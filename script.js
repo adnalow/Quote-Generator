bb315ee8aaa0a6ee6703cf6683e7aa306e3499a8
Adrian <reinieradrianluna@gmail.com>
2023-08-09 17:34:16 +0800

    working in the console but not in the website

---
+++ script.js
@@ -0,0 +1,24 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+// Get Quote from API
+async function getQuote() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const quote = data[randomIndex];
+        console.log(quote);
+    } catch (error) {
+        getQuote(); // Recursively retry if there's an error
+        console.log('Oops, no quote', error);
+    }
+}
+
+// Onload
+getQuote();
